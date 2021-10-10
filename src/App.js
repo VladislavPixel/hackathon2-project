e8e8912ce9d4960330f7e4cc42cb774c88424da5
Онофрийчук Максим <onofrijcukmaksim@MacBook-Pro-Onofrijcuk.local>
2021-10-10 13:40:37 +0300

    favorites user card layot

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import { Route, Switch, Redirect } from 'react-router-dom'
-import NavBar from './components/navBar.jsx'
-import HomePage from './layots/homePage.jsx'
-import NotFaundPage from './layots/notFaundPage.jsx'
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NavBar from "./components/navBar.jsx";
+import HomePage from "./layots/homePage.jsx";
+import FavoritesUsers from "./layots/favoritesUsers";
 
 function App() {
-	return (
-		<React.Fragment>
-				<NavBar />
-				<Switch>
-					<Route path="/" exact component={HomePage} />
-					<Route path="/notfaund" component={NotFaundPage} />
-					<Redirect to="/notfaund" />
-				</Switch>
-		</React.Fragment>
-	)
+  return (
+    <React.Fragment>
+      <NavBar />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/favorites" component={FavoritesUsers} />
+        <Redirect to="/notfaund" />
+      </Switch>
+    </React.Fragment>
+  );
 }
-
-export default App
+export default App;
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -5,7 +5,10 @@ const Button = ({color, nameBtn, typeForm}) => {
 		const styles = {fontSize: '20px',}
 		if(form === 'sharp'){
 			styles.borderRadius = '0px'
-		}
+		} else if (form === 'remove') {
+         styles.width = "100%"
+         styles.height = "40%"
+      }
 		return styles
 	}
 	return <button style = {getFormBtn(typeForm)} type="button" className={`btn btn-${color}`}>{nameBtn}</button>
